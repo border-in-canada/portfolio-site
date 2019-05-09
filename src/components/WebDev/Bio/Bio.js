046916134790129b46b0ab6e166008b98fe22249
border-in-canada <mathesd84@gmail.com>
2019-05-08 18:26:17 -0700

    Added width fixes

--- src/components/WebDev/Bio/Bio.js
+++ src/components/WebDev/Bio/Bio.js
@@ -6,9 +6,12 @@ const bio = () => (
     <div className={classes.Container}>
         <div name='about' className={classes.Bio}>
             <figure>
-                <img src={image} alt='Pic of DM' />
+                <div className={classes.Figure}>
+                    <img src={image} alt='Pic of DM' />
+                    
+                </div>
             </figure>
-            <p>I'm David Mathes, a Front-end web developer with a background in IT. I'm a curious, fast-paced programmer with a knack for tinkering and figuring out how things work. I also strongly believe in open-source software and the community it fosters. When I'm not programming you can find me engineering records and playing drums:)<br /><br />Are you looking for someone to help you with your website? Check out my projects here and reach out if you'd like to talk about your project.</p>
+            <p className={classes.Words}>I'm David Mathes, a Front-end web developer with a background in IT. I'm a curious, fast-paced programmer with a knack for tinkering and figuring out how things work. I also strongly believe in open-source software and the community it fosters. When I'm not programming you can find me engineering records and playing drums:)<br /><br />Are you looking for someone to help you with your website? Check out my projects here and reach out if you'd like to talk about your project.</p>
         </div>            
     </div>
     
